1495f0822e5d091492ab9538210f1a41bee2792c
Will Bogen <willb410@gmail.com>
2019-02-01 17:07:49 -0500

    Imported data into server

--- startbootstrap-scrolling-nav-gh-pages/app.py
+++ startbootstrap-scrolling-nav-gh-pages/app.py
@@ -4,27 +4,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask import Flask, jsonify,render_template
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-#import mysql_conn
+from mysql_conn import password
 import numpy as np
 import pymysql
-#pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
-#connection_string = (
- #   f"root:{mysql_conn.password}@localhost/apps_db")
+connection_string = (f"root:{password}@localhost/real_estate")
+engine = create_engine(f"mysql://{connection_string}")
 #engine = create_engine('sqlite://cities.sqlite')
 
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-##Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect=True)
 # Save reference to the table
-##Apps = Base.classes.apps
-##description = Base.classes.apple_description
-##print(Base.classes.keys())
+mean_sales_count = Base.classes.mean_sales_count
+median_sales_count = Base.classes.median_sales_count
+median_price_sqft = Base.classes.median_price_sqft
+median_price_zip = Base.classes.median_price_zip
+print(Base.classes.keys())
 
 # Create our connection object
-##session = Session(engine)
+session = Session(engine)
 
 #################################################
 # Flask Setup
